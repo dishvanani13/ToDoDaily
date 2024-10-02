dc4f76c67f2ccd65a2ade7857c8cb442b67c2afd
dishavanani13 <dishvanani13@gmail.com>
2024-10-01 21:39:36 -0400

    create table list

--- DoDaily/ToDoDailyViewController.swift
+++ DoDaily/ToDoDailyViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ToDoDailyViewController: UITableViewController {
 
+    //MARK - Variables
+    
+    let itemArray = ["milk","find ring", "grocery","garbage bag"]
+    //MARK - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //TableView Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItem", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
 }
 
