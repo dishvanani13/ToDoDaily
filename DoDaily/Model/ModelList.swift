dcaa0281520892b43cdfb83bc0ae4e117e38dfde
dishavanani13 <dishvanani13@gmail.com>
2024-10-04 09:17:18 -0400

    setup into MVC

---
+++ DoDaily/Model/ModelList.swift
@@ -0,0 +1,14 @@
+//
+//  ModelList.swift
+//  DoDaily
+//
+//  Created by Disha Limbani on 2024-10-04.
+//
+
+import Foundation
+
+
+class ModelList {
+    var item : String = ""
+    var done : Bool = false
+}
