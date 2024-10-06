856bf76587e7aac6bc06cbc6fb797a91a75a285c
dishavanani13 <dishvanani13@gmail.com>
2024-10-06 13:36:47 -0400

    Reuseable cell checkmark problem solve

--- DoDaily/Controller/ToDoDailyViewController.swift
+++ DoDaily/Controller/ToDoDailyViewController.swift
@@ -11,16 +11,20 @@ class ToDoDailyViewController: UITableViewController {
 
     //MARK - Variables
     
-    var arryItem = ["milk","find ring", "grocery","garbage bag"]
+    var arryItem = [Item]()
     let userDefaults = UserDefaults.standard
     //MARK - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        if let data = userDefaults.array(forKey: "ToDoDaily") as? [String] {
-            arryItem = data
-        }
+//        if let data = userDefaults.object(forKey: "ToDoDaily") as? [String] {
+//            arryItem = data
+//        }
+        let newItem = Item()
+        newItem.title = "Mixer"
+        arryItem.append(newItem)
+        
     }
 
     //TableView Methods
@@ -29,17 +33,28 @@ class ToDoDailyViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItem", for: indexPath)
-        cell.textLabel?.text = arryItem[indexPath.row]
+        cell.textLabel?.text = arryItem[indexPath.row].title
+        if arryItem[indexPath.row].done == true{
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+       
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        if arryItem[indexPath.row].done == false {
+            arryItem[indexPath.row].done = true
+        }else {
+            arryItem[indexPath.row].done = false
         }
-        
+        //if arryItem[in]
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -51,9 +66,10 @@ class ToDoDailyViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            if let data = textField.text {
-                self.arryItem.append(data)
-            }
+            let newitem = Item()
+            newitem.title = textField.text!
+            self.arryItem.append(newitem)
+           
             self.userDefaults.setValue(self.arryItem, forKey: "ToDoDaily")
             self.tableView.reloadData()
         }
--- DoDaily/Model/Item.swift
+++ DoDaily/Model/Item.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-class ModelList {
-    var item : String = ""
+class Item {
+    var title : String = ""
     var done : Bool = false
 }
